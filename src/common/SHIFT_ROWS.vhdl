e16839c5c49cd43a15ed6c24c0e27426f7d4c94c
akoerner <andrew@k0ner.com>
2013-03-15 14:14:23 -0500

    shift rows compiles not tested

--- src/common/SHIFT_ROWS.vhdl
+++ src/common/SHIFT_ROWS.vhdl
@@ -1,35 +1,41 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
 entity ShiftRows is
-	Port ( State_Input : in  STD_LOGIC_VECTOR (127 downto 0);
-	   State_Output : out  STD_LOGIC_VECTOR (127 downto 0);
-	   SYS_CLK : in  STD_LOGIC);
+	Port (
+			STATE_IN : in  STD_LOGIC_VECTOR (127 downto 0);
+			State_OUT : out  STD_LOGIC_VECTOR (127 downto 0);
+			CLOCK : in  STD_LOGIC
+		 );
 end ShiftRows;
 
 architecture Behavioral of ShiftRows is
 
 begin
-SHIFT_ROWS : PROCESS(SYS_CLK)
-begin
-	IF (SYS_CLK'event AND SYS_CLK ='1') then
-		State_Output <= State_Input(127 downto 120) &
-						State_Input(87 downto 80)   &
-						State_Input(47 downto 40)   &
-						State_Input(7 downto 0)     &
+SHIFT_ROWS : PROCESS(CLOCK)
+	begin
+		IF (CLOCK'event AND CLOCK ='1') then
+			STATE_OUT <= STATE_IN(127 downto 120) &
+							STATE_IN(87 downto 80)   &
+							STATE_IN(47 downto 40)   &
+							STATE_IN(7 downto 0)     &
+								
+							STATE_IN(95 downto 88)   &
+							STATE_IN(55 downto 48)   &
+							STATE_IN(15 downto 8)    &
+							STATE_IN(103 downto 96)  &
 							
-						State_Input(95 downto 88)   &
-						State_Input(55 downto 48)   &
-						State_Input(15 downto 8)    &
-						State_Input(103 downto 96)  &
-	
-						State_Input(63 downto 56)   &
-						State_Input(23 downto 16)   &
-						State_Input(111 downto 104) &
-						State_Input(71 downto 64)   &
-																			
-						State_Input(31 downto 24)   &
-						State_Input(119 downto 112) &
-						State_Input(79 downto 72)   &
-						State_Input(39 downto 32)   ;                                                
-	END IF;
-END PROCESS;    
-
+							STATE_IN(63 downto 56)   &
+							STATE_IN(23 downto 16)   &
+							STATE_IN(111 downto 104) &
+							STATE_IN(71 downto 64)   &
+																				
+							STATE_IN(31 downto 24)   &
+							STATE_IN(119 downto 112) &
+							STATE_IN(79 downto 72)   &
+							STATE_IN(39 downto 32);                                                
+		END IF;
+	END PROCESS;    
 end Behavioral;
+
+
